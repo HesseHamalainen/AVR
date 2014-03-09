3ce18253d3cee135b0502fd844ec2512ddb84d99
Hesse Hamalainen <hesse@realisaatio.fi>
2014-03-09 16:49:34 +0200

    Cleanup

--- PF_Tek1_3.ino
+++ PF_Tek1_3.ino
@@ -1,5 +1,5 @@
 /*
- * PF_Tek1.2
+ * PF_Tek1.3
  *
  * 9.3.2014
  *
@@ -26,8 +26,6 @@
  *
  */
 
-#include <OneWire.h>
-#include <DS18B20.h>
 #include <EEPROMex.h>
 #include <EEPROMVar.h>
 
@@ -45,11 +43,6 @@
 #define AIR_DURATION 120 // for 2 minutes
 #define UPTIME_STORE_INTERVAL 60 // maximum 60 seconds uptime loss
 
-#define PIN_TEMPERATURE 7
-#define PIN_HUMIDITY 0
-
-DS18B20 Temperature(PIN_TEMPERATURE);
-
 uint32_t uptime = 0;
 uint32_t uptimeStoreTime = 0;
 uint32_t localUptime = 0;
@@ -64,32 +57,11 @@ uint8_t airStatus = 0;
 uint32_t airStart = 0;
 uint32_t timeSinceAir = 0;
 
-static byte temperatureSensors[1][8];
-boolean conversionRunning = false;
-unsigned long conversionStart = 0;
-
-struct Temperature {
-  
-  byte *Address;
-  float Temperature;
-  unsigned long updateTime;
-  
-};
-
-struct Environment {
-  
-  float Humidity;
-  float HumidityRaw;
-  struct Temperature Temperatures[1];
-  
-} Environment;
-
 void setup(void) {
   initEepromAddressing();
   initUptime();
   initAir();
   initSerial();
-  initTemperature();
 }
 
 void initEepromAddressing(void) {
@@ -112,27 +84,11 @@ void initSerial(void) {
   Serial.begin(SERIAL_BAUD);
 }
 
-void initTemperature(void) {
-  Temperature.init(1);
-  Temperature.getAddresses(temperatureSensors);
-  
-  // Copy sensor address data into struct elements
-  for(byte i = 0;i < sizeof(Environment.Temperatures) / sizeof(struct Temperature);i++) {
-    Environment.Temperatures[i].Address = temperatureSensors[i];
-    Environment.Temperatures[i].Temperature = 0.0;
-    Environment.Temperatures[i].updateTime = 0;
-  }
-  
-}
-
 void loop(void) {
   #ifdef DEBUG
     Serial.println("loop");
   #endif
-  
-  envTemperature();
-  envHumidity();
-  
+
   updateLocalUptime();
   
   airStatus = EEPROM.readByte(addressAirStatus);
@@ -238,73 +194,6 @@ void storeUptime(void) {
   #ifndef DISABLE_WRITE
     EEPROM.writeLong(addressUptime, uptime);
   #endif
-}
-
-void envTemperature() {
-  
-  // Temperature requires 800ms delay time for each sensor
-  // Because we don't want to use delay() to halt main loop(), we use software
-  // to track if that 800ms has passed. Then we read the temperature.
-  //
-  // This needs to be done for each sensor, at separate times.
-  // We loop through each sensor, always getting the one that hasn't been updated for the longest time
-  // Then we either start conversion (and tracking of 800ms), or, if the conversion has finished (800ms passed)
-  // we read the temperature, and move to next device.
-  //
-  // I don't know, if OneWire / DS18B20 supports requesting ALL devices to do the temperature
-  // conversion at once, and then reading them all; until I know better, this will have to do.
-  
-  struct Temperature *TemperatureDevice = getTemperatureNextDevice();
-
-  if (conversionRunning) {
-
-    if ((millis() - conversionStart) > 800) {
-      (*TemperatureDevice).Temperature = Temperature.getTemp((*TemperatureDevice).Address);
-      (*TemperatureDevice).updateTime = millis();
-        
-      conversionRunning = false;
-    }
-    else {
-      // Let the good times roll.. a bit more
-    }
-  }
-  else {
-    Temperature.startConversion((*TemperatureDevice).Address, false);
-      
-    conversionRunning = true;
-    conversionStart = millis();
-  }
-  
-}
-
-struct Temperature *getTemperatureNextDevice(void) {
-  unsigned long leastTime = 0;
-  int leastId = -1;
-  
-  for(byte i = 0;i < sizeof(Environment.Temperatures) / sizeof(struct Temperature);i++) {
-    if (leastId == -1) {
-      leastId = i;
-      leastTime = Environment.Temperatures[i].updateTime;
-    }
-    else {
-      if (Environment.Temperatures[i].updateTime <= leastTime) {
-        leastId = i;
-        leastTime = Environment.Temperatures[i].updateTime;
-      }
-    }
-  }
-  
-  return &Environment.Temperatures[leastId];
-}
-
-void envHumidity() {
-  float supplyVolt = 5.0;
-
-  int HIH4030_Value = analogRead(PIN_HUMIDITY);
-  float voltage = HIH4030_Value/1023. * supplyVolt;
-
-  Environment.HumidityRaw = 161.0 * voltage / supplyVolt - 25.8;
-  Environment.Humidity = Environment.HumidityRaw / (1.0546 - 0.0026 * Environment.Temperatures[0].Temperature); // Temperature adjustment 
 }
 
 void serialPrint(void) {
